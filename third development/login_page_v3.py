863f3cfc8d274e6632dc811ef9e81064057cce91
mariasecara082 <mariasecara082@gmail.com>
2025-07-16 10:50:40 +1200

    Changing to custom tkinter
    I am changing my code to custom tkinter to ensure the app looks much nicer

--- third development/login_page_v3.py
+++ third development/login_page_v3.py
@@ -1,8 +1,6 @@
 '''this will be used as the login page.
 all functions coded will be put here (GUI and back end python)'''
-from tkinter import *
-import webbrowser
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import messagebox
 import subprocess
 import sys
@@ -58,18 +56,18 @@ def createnew_popup():
     to register a new user is pressed. the window
     has certain dimensions, just like the root'''
 
-    popupwindow = Toplevel(root)
+    popupwindow = ctk.CTkToplevel(root)
     popupwindow.title("Register new user")
     popupwindow.geometry("500x300")
     popupwindow.configure(bg="#fffef8")
     popupwindow.iconbitmap("images/logo.ico")
 
-    Label(popupwindow, text="Create username:", font=("Arial", 13), bg="mediumpurple").pack(pady=5)
-    register_username_entry = Entry(popupwindow)
+    ctk.CTkLabel(popupwindow, text="Create username:", font=("Arial", 13)).pack(pady=5)
+    register_username_entry = ctk.CTkEntry(popupwindow)
     register_username_entry.pack(pady=5)
 
-    Label(popupwindow, text="Create password: (8-20 characters long)", font=("Arial", 13), bg="mediumpurple").pack(pady=5)
-    register_password_entry = Entry(popupwindow, show="•")
+    ctk.CTkLabel(popupwindow, text="Create password: (8-20 characters long)", font=("Arial", 13)).pack(pady=5)
+    register_password_entry = ctk.CTkEntry(popupwindow, show="•")
     register_password_entry.pack(pady=5)
 
     def save_new_user(): 
@@ -107,41 +105,44 @@ def createnew_popup():
         except Exception as e:
             messagebox.showerror("Error", f"Failed to save user:\n{e}")
 
-    Button(popupwindow, text="Create New User", command=save_new_user, bg="red", fg="mediumPurple").pack(pady=20)
+    ctk.CTkButton(popupwindow, text="Create New User", command=save_new_user, fg_color="red", text_color="red").pack(pady=20)
 
     
     #new_user = new_user_name.get().strip()
 
 
 #Creating a main window using root = tk.Tk().
-root = tk.Tk()
+root = ctk.CTk()
 root.title("Secret Self Diary App")
-root.geometry("1400x900")
-root.configure(bg="#fffef8") #Making the root colour have a background colour.
+root.geometry("1000x700")
+root.configure(fg_color="#fffef8") #Making the root colour have a background colour.
 root.iconbitmap("images/logo.ico")
 
 #Welcome message displayed at the top of the root.
-welcome_message = tk.Label(root, text="*:･ﾟ✧*:･ﾟ Welcome to Secret Self Diary App *:･ﾟ✧*:･ﾟ", font=('Helvetica', 26), bg="#fffef8", fg="#7c5b44")
-welcome_message.place(relx=0.5, rely=0.15, anchor="center")
+welcome_message = ctk.CTkLabel(root, text="Secret Self Diary App", font=('Helvetica', 32), fg_color="#fffef8", text_color="#7c5b44")
+welcome_message.place(relx=0.5, rely=0.1, anchor="center")
+
+#Login message below the welcome message
+login_message = ctk.CTkLabel(root, text="Login", font=('Helvetica', 28), fg_color="#fffef8", text_color="#7c5b44")
+login_message.place(relx=0.5, rely=0.16, anchor="center")
 
 
 #Label for the user to enter their username
-name = Label(root, text="Username", font=("Arial", 13), bg="mediumpurple")
-name.place(relx=0.4, rely=0.25)
-user_name = Entry(root, font=("Arial", 11), width=35)
-user_name.place(relx=0.4, rely=0.3)
+user_name = ctk.CTkEntry(root, font=("Arial", 13), width=300, placeholder_text="Username")
+user_name.place(relx=0.4, rely=0.25)
 
 #Label for the user to enter their password.
-password = Label(root, text="Password (8-20 characters long)", font=("Arial", 13), bg="mediumpurple").pack(pady=(20,3))
-user_password = Entry(root, font=("Arial", 11), show="•", width=35)
-user_password.pack(pady=(20,2))
+user_password = ctk.CTkEntry(root, font=("Arial", 13), show="•", width=300, placeholder_text="Password")
+user_password.place(relx=0.4, rely=0.33)
 
 #Making a submit button for the login page, saving it to a text file.
-submit_info = Button(root, text="Log In", font=("arial", 15), command=enter_user_name, bg="red", fg="mediumPurple").place(x=400,y=250)
+submit_info = ctk.CTkButton(root, text="Log In", font=("arial", 13), command=enter_user_name, fg_color="red", text_color="#7c5b44")
+submit_info.place(relx=0.46, rely=0.4)
 
 #Creating a button where the user can register and make a new account.
 
-register_user = Button(root, text="Register new user:", font=("arial", 15), command=createnew_popup, bg="mediumPurple", fg="mediumPurple").place(x=180,y=250)
+register_user = ctk.CTkButton(root, text="Register new user:", font=("arial", 13), command=createnew_popup, text_color="#7c5b44")
+register_user.place(relx=0.36, rely=0.46)
 
 
 root.mainloop()
