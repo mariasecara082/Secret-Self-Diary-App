ac44c0be9730bd90879d622879e2c9cf5527eb4f
mariasecara082 <mariasecara082@gmail.com>
2025-07-20 08:48:15 +1200

    Changes
    - creating the questionnaire database
    - updating the user database
    - making a separate folder for all of the customtkinter changes made.

---
+++ custom tkinter/daily_qs_ct.py
@@ -0,0 +1,208 @@
+'''this will be used as the questionnaire page.
+all functions coded will be put here (GUI and back end python)'''
+
+'''Version Three (V3, this version), is the version with changed GUI, as well
+as changed transitioning between the questions inside the quenstionnaire.
+Instead of using a new window to change between the questions, a new
+frame will be made. The option to go back to the old  question to
+give the users freedom to change their answers. The final answers will
+only be seen on the textfile once the whole questionnaire has been submitted'''
+
+import customtkinter as ctk
+from tkinter import messagebox, IntVar
+from PIL import Image, ImageTk
+import subprocess
+import sys
+
+#Creating a dictionary with the options in the questionnaire.
+questionnaire_choices = [
+    " Calm 😌"," Curious 👀"," Bored 🥱"," Annoyed 😑",
+    " Stressed 😰"," Joyful 😄", " Grateful 🥹", " Lonely 😔", 
+    " Ashamed 🙈", " Tired 😴", " Disappointed 🫠", " Peaceful ✌️"]
+
+#Function where the users can submit their choices.
+def q1_answers():
+
+    '''Here is the back ended code needed for
+    creating the first question. Below, there is the selected_feelings,
+    which is a list of all the options the user can choose. There is also
+    a try and except to save the choices into a text file, and there is an if
+    statement to warn the user that no option has been selected.'''
+
+    selected_feelings = [choice for choice, var in checkbox_vars.items() if var.get() == 1]
+    
+    try:
+        with open("daily_questionnaire.txt", "a") as f:
+            f.write(f"{selected_feelings}\n")
+    except Exception as e:
+        messagebox.showerror("Error", f"Something went wrong. Please try again!\n{e}")
+    if not selected_feelings:
+        messagebox.showwarning("No selection", "Please select at least one option.")
+        return
+    print("User feelings submitted")
+    root.destroy()
+    q2_window_open()
+
+def q2_answers():
+    hilite_day = q2_entry.get().strip()
+
+    try:
+        with open("daily_questionnaire.txt", "a") as f:
+            f.write(f"Highlight: {hilite_day}\n")
+    except Exception as e:
+        messagebox.showerror("Error", f"Something went wrong. Please try again!\n{e}")
+    if not hilite_day:
+        messagebox.showerror("Error", f"Please enter a response and try again\n{e}")
+        return
+    print("Highlight of day submitted")
+    q2_window.destroy()
+    q3_window_open()
+
+
+def q2_window_open():
+
+    '''This def will be used to create the GUI for
+    the second question in the questionnaire. It opens right after
+    the root window (which belongs to question 1) 
+    get destroyed.'''
+
+    global q2_window, q2_entry
+    q2_window = ctk.CTk()
+    q2_window.title("Secret Self Diary App")
+    q2_window.geometry("1400x900")
+    q2_window.configure(bg="#fffef8") #Making the root colour have a background colour.
+    q2_window.iconbitmap("images/logo.ico")
+
+    welcome_message = ctk.CTkLabel(q2_window, text="Welcome back, username", font=("Biski", 25), bg="#fffef8", fg="#7c5b44")
+    welcome_message.place(relx=0.5, rely=0.15, anchor="center")
+
+    progressq2 = ctk.CTkProgressbar(q2_window, orient="horizontal", length=400, mode="determinate")
+    progressq2.place(relx=0.5, rely=0.05, anchor="center")
+    progressq2["value"] = 66
+
+    q2_label = ctk.CTkLabel(q2_window, text="What was the highlight of your day?", font=("Arial", 20), bg="#fffef8", fg="#898686")
+    q2_label.place(relx=0.5, rely=0.22, anchor="center")
+    
+    q2_entry = ctk.CTkEntry(q2_window, fg="#898686", width=50, font=("Arial", 14))
+    q2_entry.place(relx=0.5, rely=0.4, anchor="center")
+
+    q2_submit_button = ctk.CTkButton(q2_window, text="Submit", font=("Arial", 14), bg="#f4c430", command=q2_answers)
+    q2_submit_button.place(relx=0.75, rely=0.85, relwidth=0.1, relheight=0.05)
+    
+    q2_window_open.mainloop()
+
+def q3_answers():
+
+    '''This def will be used to create the
+    back ended things needed for the third question of the code.'''
+
+    rating = q3_slider.get()
+    try:
+        with open("daily_questionnaire.txt", "a") as f:
+            f.write(f"Day rating: {rating}/10\n")
+        #Destroying and closing this window
+        q3_window.destroy()
+        script_path = "/Users/maria/Desktop/13DDT/13DDT-PROG-MariaSecara/Secret Self Diary App/second development/homepage_v2.py" 
+        '''opening up the daily questionnaire'''
+        subprocess.Popen([sys.executable, script_path])
+    except Exception as e:
+        messagebox.showerror("Error", f"Something went wrong, please try again:\n{e}")
+
+
+def q3_window_open():
+
+    '''This def will be used to create the GUI for
+    the third question in the questionnaire. It opens right after
+    the q2 window (which belongs to question 2) 
+    get destroyed.'''
+
+    global q3_window, q3_slider
+    q3_window = ctk.CTk()
+    q3_window.title("Secret Self Diary App")
+    q3_window.geometry("1400x900")
+    q3_window.configure(bg="#fffef8") #Making the root colour have a background colour.
+    q3_window.iconbitmap("images/logo.ico")
+
+    welcome_message = ctk.CTkLabel(q3_window, text="Welcome back, username", font=("Biski", 25), bg="#fffef8", fg="#7c5b44")
+    welcome_message.place(relx=0.5, rely=0.15, anchor="center")
+
+    progressq3 = ctk.CTkProgressbar(q3_window, orient="horizontal", length=400, mode="determinate")
+    progressq3.place(relx=0.5, rely=0.05, anchor="center")
+    progressq3["value"] = 100
+
+    q3_label = ctk.CTkLabel(q3_window, text="Rate your day on a scale from 1-10:", font=("Arial", 20), bg="#fffef8", fg="#898686")
+    q3_label.place(relx=0.5, rely=0.22, anchor="center")
+
+    q3_slider = ctk.CTkScale(q3_window, from_=0, to=10, orient=HORIZONTAL, length=500, tickinterval=0.5, font=("Arial", 12))
+    q3_slider.set(5)
+    q3_slider.place(relx=0.32, rely=0.4)
+    q3_submit_button = ctk.CTkButton(q3_window, text="Submit", font=("Arial", 14), bg="#f4c430", command=q3_answers)
+    q3_submit_button.place(relx=0.75, rely=0.85, relwidth=0.1, relheight=0.05)
+    
+    q3_window_open.mainloop()
+
+#Creating the main window in which the quiz will be located.
+root = ctk.CTk()
+root.title("Secret Self Diary App")
+root.geometry("1400x900")
+root.configure(bg="#fffef8") #Making the root colour have a background colour.
+root.iconbitmap("images/logo.ico") #Adding the logo
+
+#Making a frame to outline the questionnaire
+#This will take away from the whitespace a bit
+main_frame = ctk.CTkFrame(root, bg="#ffffff")
+
+#Images for the checked and unchecked checklist boxes
+def flatten_transparent_png(image_path, bg_color="#fffef8"):
+    im = Image.open(image_path).convert("RGBA")
+    background = Image.new("RGBA", im.size, bg_color)
+    alpha = im.split()[-1]
+    background.paste(im, mask=alpha)
+    return background.convert("RGB")
+
+unchecked_pil = flatten_transparent_png("untickedbox.png").resize((48, 48), Image.Resampling.LANCZOS)
+checked_pil = flatten_transparent_png("tickedbox.png").resize((48, 48), Image.Resampling.LANCZOS)
+
+unchecked_image = ImageTk.PhotoImage(unchecked_pil)
+checked_image = ImageTk.PhotoImage(checked_pil)
+
+welcome_message = ctk.CTkLabel(root, text="Welcome back, username", font=("Biski", 25), bg="#fffef8", fg="#7c5b44")
+welcome_message.place(relx=0.5, rely=0.15, anchor="center")
+
+q1_label = ctk.CTkLabel(root, text="How are you feeling today?", font=("Arial", 20), bg="#fffef8", fg="#898686")
+q1_label.place(relx=0.5, rely=0.22, anchor="center")
+
+progressq1 = ctk.CTkProgressbar(root, orient="horizontal", length=400, mode="determinate")
+progressq1.place(relx=0.5, rely=0.05, anchor="center")
+progressq1["value"] = 33
+
+#Create checkboxes.
+checkbox_vars = {}
+for idx, choice in enumerate(questionnaire_choices):
+    var = IntVar()
+    
+    
+    #Positioning in 3 rows and 3 columns.
+    col = idx % 3
+    row = idx // 3
+
+    checkbox = ctk.CTkCheckbutton(#Images
+    text=choice,
+    variable=var,
+    image=unchecked_image,
+    selectimage=checked_image,
+    compound="left", #Moves the image to the left of the text
+    indicatoron=False,  #Removes small default box
+    bg="#fffef8"
+    )
+    checkbox.place(relx=0.15 + col * 0.25, rely=0.4 + row * 0.08, relwidth=0.2, relheight=0.1)
+    checkbox_vars[choice] = var
+    
+    checkbox.image = unchecked_image 
+    checkbox.selectimage = checked_image
+
+#Making the submit buttons for the questions
+q1_submit_button = ctk.CTkButton(root, text="Next", font=("Arial", 15), bg="mediumpurple", command=q1_answers)
+q1_submit_button.place(relx=0.75, rely=0.85, relwidth=0.1, relheight=0.05)
+
+root.mainloop()
---
+++ custom tkinter/login_page_ct.py
@@ -0,0 +1,187 @@
+'''this will be used as the login page.
+all functions coded will be put here (GUI and back end python)'''
+import customtkinter as ctk
+from tkinter import messagebox
+import subprocess
+import sys
+
+#Here, I am starting to code the GUI for the login page. For the sake of this testing, use the username test and test123.
+def enter_user_name():
+
+    '''main function (on the login page)
+    in which the user enters their username and password'''
+    
+    username = user_name.get().strip() #.strip() is used to remove any trailing whitespaces (spaces, tabs, new lines)
+    password = user_password.get().strip()
+
+    # Check for empty fields.
+    if not username or not password:
+        messagebox.showwarning("Validation Error", "Username and password cannot be empty.")
+        return
+
+    #Checking password validity.
+    special_chars = "!@#$%^&*()}~`,.<>?/:;{[]|\+=™¡£¢∞§¶•ªºåœ∑´®†¥¨ˆøπåß∂ƒ©˙∆˚¬≈ç√∫˜µ≤≥"
+    if len(password) < 8: #Password length
+        messagebox.showwarning("Validation Error", "Password must be at least 8 characters.")
+        return
+    if len(password) > 20: #Password length
+        messagebox.showwarning("Validation Error", "Password cannot be more than 20 characters.")
+        return
+    if not any(char in special_chars for char in password): #Special characters.
+        messagebox.showwarning("Validation Error", "Password must include at least one special character (!@#$%^&*()).")
+        return
+
+    #Check for allowed usernames. - will have to change when users are getting made.
+    allowed_users = {"maria": "mariasecara1!","test":"test123@"}
+    if username not in allowed_users or allowed_users[username] != password:
+        messagebox.showerror("Login Failed", "Invalid username or password.")
+        return
+
+    # If all checks pass, save the login info
+    try:
+        with open("login_info_file.txt", "a") as f:
+            f.write(f"{username}, {password}\n")
+        messagebox.showinfo("Success", "Thank you for logging in :).")
+        #Destroying and closing this window
+        root.destroy()
+        script_path = "/Users/maria/Desktop/13DDT/13DDT-PROG-MariaSecara/Secret Self Diary App/daily_qs_ver2.py" 
+        '''opening up the daily questionnaire'''
+        subprocess.Popen([sys.executable, script_path])
+    except Exception as e:
+        messagebox.showerror("Error", f"Something went wrong, please try again:\n{e}")
+
+def createnew_popup():
+
+    '''this creates a new pop-up window when the button
+    to register a new user is pressed. the window
+    has certain dimensions, just like the root'''
+
+    popupwindow = ctk.CTkToplevel(root)
+    popupwindow.title("Register new user")
+    popupwindow.geometry("500x300")
+    popupwindow.configure(fg_color="#fffef8")
+    popupwindow.iconbitmap("images/logo.ico")
+
+    #Label
+    ctk.CTkLabel(popupwindow, 
+                 text="SIGN UP", 
+                 font=("Helvetica", 24, "bold"), 
+                 fg_color="#fffef8", 
+                 text_color="#7c5b44").place(relx=0.42, rely=0.16)
+
+    register_username_entry = ctk.CTkEntry(popupwindow,
+                             font=("Arial", 13),  
+                             width=210, 
+                             placeholder_text="Create Username", 
+                             text_color="#7c5b44", 
+                             fg_color="#fffef8")
+    register_username_entry.place(relx=0.3, rely=0.4)
+
+    register_password_entry = ctk.CTkEntry(popupwindow,
+                             font=("Arial", 13),  
+                             width=210, 
+                             placeholder_text="Create Password", 
+                             text_color="#7c5b44", 
+                             fg_color="#fffef8")
+    register_password_entry.place(relx=0.3, rely=0.57)
+
+    def save_new_user(): 
+
+        '''this function is made to save 
+        the new registered user into a textfile'''
+
+        reg_username = register_username_entry.get().strip()
+        reg_password = register_password_entry.get().strip()
+
+        #If the password or username is left empty.
+        if not reg_username or not reg_password:
+            messagebox.showwarning("Validation Error", "Username and password cannot be empty.")
+            return
+        
+        #Checking validity of the password
+        special_chars = "!@#$%^&*()}~`,.<>?/:;{[]|\+=™¡£¢∞§¶•ªºåœ∑´®†¥¨ˆøπåß∂ƒ©˙∆˚¬≈ç√∫˜µ≤≥"
+        if len(reg_password) < 8:
+            messagebox.showwarning("Validation Error", "Password must be at least 8 characters.")
+            return
+        if len(reg_password) > 20:
+            messagebox.showwarning("Validation Error", "Password cannot be more than 20 characters.")
+            return
+        if not any(char in special_chars for char in reg_password):
+            messagebox.showwarning("Validation Error", "Password must include at least one special character.")
+            return
+        
+        #Saving the new registrated user into a text file:
+
+        try:
+            with open("registered_users.txt", "a") as file:
+                file.write(f"{reg_username}, {reg_password}\n")
+            messagebox.showinfo("Success", "User registered successfully!")
+            popupwindow.destroy()
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to save user:\n{e}")
+
+    ctk.CTkButton(popupwindow, 
+                  text="Create New User", 
+                  command=save_new_user, 
+                  fg_color="#7c5b44", text_color="#fffef8", hover_color="#b59a90",
+                  width=210).place(relx=0.3, rely=0.74)
+
+    
+    #new_user = new_user_name.get().strip()
+
+
+#Creating a main window using root = tk.Tk().
+root = ctk.CTk()
+root.title("Secret Self Diary App")
+root.geometry("1400x900")
+root.configure(fg_color="#fffef8") #Making the root colour have a background colour.
+root.iconbitmap("images/logo.ico")
+
+#Welcome message displayed at the top of the root.
+welcome_message = ctk.CTkLabel(root, text="Secret Self Diary App", font=('Helvetica', 32), fg_color="#fffef8", text_color="#7c5b44")
+welcome_message.place(relx=0.5, rely=0.1, anchor="center")
+
+#Login message below the welcome message
+login_message = ctk.CTkLabel(root, text="LOGIN", font=("Helvetica", 24, "bold"), text_color="#7c5b44")
+login_message.place(relx=0.5, rely=0.16, anchor="center")
+
+
+#Label for the user to enter their username
+user_name = ctk.CTkEntry(root, 
+                         font=("Arial", 13), 
+                         width=300, 
+                         placeholder_text="Username", 
+                         text_color="#7c5b44", 
+                         fg_color="#fffef8")
+user_name.place(relx=0.4, rely=0.25)
+
+#Label for the user to enter their password.
+user_password = ctk.CTkEntry(root, 
+                             font=("Arial", 13), 
+                             show="•", 
+                             width=300, 
+                             placeholder_text="Password", 
+                             text_color="#7c5b44", 
+                             fg_color="#fffef8")
+user_password.place(relx=0.4, rely=0.33)
+
+#Making a submit button for the login page, saving it to a text file.
+submit_info = ctk.CTkButton(root, 
+                            text="LOGIN", 
+                            command=enter_user_name, 
+                            fg_color="#7c5b44", 
+                            text_color="#fffef8", 
+                            hover_color="#b59a90",
+                            width=300)
+submit_info.place(relx=0.4, rely=0.4)
+
+#Creating a button where the user can register and make a new account.
+
+signup_text = ctk.CTkLabel(root, text="Don't have an account? Sign up here", text_color="#7c5b44", cursor="hand2", font=("Arial", 12, "underline"))
+signup_text.place(relx=0.44, rely=0.45)
+signup_text.bind("<Button-1>", lambda e: createnew_popup())
+
+
+root.mainloop()
+
+#create_gui()
